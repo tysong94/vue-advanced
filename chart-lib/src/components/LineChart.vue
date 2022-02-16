60c86b5be22bf8b03892c19f97ed934e0ce2487d
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-02-16 22:55:28 +0900

    LineChart Componentization

---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <canvas id="myChart2"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  mounted() {
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChart = new Chart(
+      document.getElementById('myChart2'),
+      config
+    );
+  }
+}
+</script>
+
+<style>
+
+</style>
