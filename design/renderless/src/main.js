3c71163e7f69fe5d84683bb0ef1a0fe452c3bd2a
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-02-28 16:31:17 +0900

    Renderless Component Complete

--- design/renderless/src/main.js
+++ design/renderless/src/main.js
@@ -4,13 +4,13 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  /*   
   // 1
   render: function(createElement) {
     return createElement(App);
   },
   // 2
-  render: function(h) {
+  render: function(h) { // h: hypertext의 약자
     return h(App);
   },
   // 3
@@ -18,5 +18,7 @@ new Vue({
     return h(App);
   },
   // 4
+  render: h => h(App), 
+  */
   render: h => h(App),
 }).$mount('#app')
